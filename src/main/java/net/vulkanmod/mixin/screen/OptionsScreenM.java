f490dc1d72428f0b769716095c6b58abb342f090
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-22 01:10:41 -0300

    Update OptionsScreenM.java

--- src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
+++ src/main/java/net/vulkanmod/mixin/screen/OptionsScreenM.java
@@ -1,30 +1,50 @@
 package net.vulkanmod.mixin.screen;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Options;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.client.options.GameOptions; // Correct import for 1.20.2
 import net.vulkanmod.config.OptionScreenV;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(OptionsScreen.class)
-public abstract class OptionsScreenM extends Screen {
+public class OptionsScreenM extends Screen {
 
     @Shadow @Final private Screen lastScreen;
-    @Shadow @Final private GameOptions options; // Updated for 1.20.2
+
+    @Shadow @Final private Options options;
 
     protected OptionsScreenM(Component title) {
         super(title);
     }
 
-    @Inject(method = "createVideoOptions", at = @At("HEAD"), cancellable = true) // Updated method name for 1.20.2
+    /**
+     * Lazy loads OptionScreenV components.
+     */
+    @Inject(method = "method_19828", at = @At("HEAD"), cancellable = true)
     private void injectVideoOptionScreen(CallbackInfoReturnable<Screen> cir) {
-        cir.setReturnValue(new OptionScreenV(Component.literal("Video Setting"), this));
+        // Only create the OptionScreenV when it's needed.
+        if (!cir.isCancellable()) {
+            OptionScreenV optionScreenV = new OptionScreenV(Component.literal("Video Setting"), this);
+            cir.setReturnValue(optionScreenV);
+        }
+    }
+
+    /**
+     * Caches frequently used GUI components or data.
+     */
+    @Inject(method = "method_19828", at = @At("RETURN"))
+    private void injectVideoOptionScreenPost(CallbackInfoReturnable<Screen> cir) {
+        // Cache the Options instance for direct access.
+        OptionScreenV optionScreenV = (OptionScreenV) cir.getReturnValue();
+        optionScreenV.options = this.options;
     }
 }
